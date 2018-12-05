b5967ad2a97572a3757157fb25c141b23a21ce37
bry-an <bbyunis@gmail.com>
2018-12-04 20:02:03 -0700

    add map component, store structure

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="google-map" ref="map"></div>
+</template>
+
+<script>
+export default {
+  name: "GoogleMap",
+  data() {
+    return {
+      map: null,
+      center: null,
+      markers: []
+    };
+  },
+  computed: {
+    mapCenter() {
+      return this.$store.getters.getMapCenter;
+    }
+  },
+  methods: {
+    initMap() {
+      const mapRef = this.$refs.map;
+      const options = {
+        zoom: 14,
+        center: this.mapCenter
+      };
+    }
+  }
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <google-map>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import GoogleMap from "../components/GoogleMap"
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    GoogleMap
   }
 }
 </script>
