3bdf09452b66353e0771dc3a2f330362f77616b7
Bryan Yunis <bbyunis@gmail.com>
2018-12-13 22:12:40 -0700

    add DirectionsRenderer component

---
+++ src/components/DirectionsRenderer.vue
@@ -0,0 +1,20 @@
+<template></template>
+
+<script>
+export default {
+  name: "DirectionsRendered",
+  props: {
+    route: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    directionsRenderer() {
+      const directionsDisplay = new google.maps.directionsRenderer({
+        map: this.$store
+      });
+    }
+  }
+};
+</script>
